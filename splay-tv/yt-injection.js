cfe40b5c9d8c4abe8a3517cd6f0d97a0019766b2
Simas2006 <sjuknelis@gmail.com>
2018-12-23 16:21:08 -0500

    Injection script to YouTube watch page

---
+++ splay-tv/yt-injection.js
@@ -0,0 +1,17 @@
+var {ipcRenderer} = require("electron");
+
+window.onload = function() {
+  var player = document.getElementById("movie_player");
+  var interval = setInterval(function() {
+    if ( player.getCurrentTime() + 0.5 >= player.getDuration() ) {
+      ipcRenderer.sendToHost("video-end");
+      clearInterval(interval);
+    }
+  },1000);
+  ipcRenderer.on("video-command",function(obj,command,param) {
+    if ( command == "play" ) player.playVideo();
+    else if ( command == "pause" ) player.pauseVideo();
+    else if ( command == "setvol" ) player.setVolume(param);
+    else if ( command == "rewind" ) player.seekTo(0);
+  });
+}
