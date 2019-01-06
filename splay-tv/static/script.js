e9a399997d128912d8c7e192ec09705d617da250
Simas2006 <sjuknelis@gmail.com>
2019-01-06 16:07:42 -0500

    Properly handle empty albums in selector

--- splay-tv/static/script.js
+++ splay-tv/static/script.js
@@ -3,7 +3,6 @@ var DATA_FOLDER = __dirname + "/../data";
 
 var currentPage = "home";
 var queue = [];
-var aa; // Audio Agent
 var pf = { // Page Functions
   "home": {
     "load": Function.prototype,
@@ -186,48 +185,65 @@ var pf = { // Page Functions
       });
     },
     "renderLinks": function() {
+      function merge(list,isDir) {
+        if ( ! isDir ) {
+          if ( ! pf.mlibrary.selected ) pf.mlibrary.selected = list.map(item => false);
+          document.getElementById("mlibrary-button1").style.display = "inline";
+          document.getElementById("mlibrary-button2").style.display = "inline";
+          if ( pf.mlibrary.selected.filter(item => ! item).length > 0 || list.length <= 0 ) document.getElementById("mlibrary-button1").innerText = "Select All";
+          else document.getElementById("mlibrary-button1").innerText = "Deselect All";
+        } else {
+          document.getElementById("mlibrary-button1").style.display = "none";
+          document.getElementById("mlibrary-button2").style.display = "none";
+        }
+        var div = document.getElementById("mlibrary-items");
+        while ( div.firstChild ) {
+          div.removeChild(div.firstChild);
+        }
+        for ( var i = 0; i < list.length; i++ ) {
+          var a = document.createElement("a");
+          if ( ! isDir ) {
+            for ( var j = 0; j < list[i].length; j++ ) {
+              if ( "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".indexOf(list[i].charAt(j)) > -1 ) break;
+            }
+            list[i] = list[i].slice(j).split(".").slice(0,-1).join(".");
+          }
+          a.innerText = list[i];
+          if ( ! isDir ) a.style.color = pf.mlibrary.selected[i] ? "#00aa00" : "blue";
+          a["data-index"] = i;
+          a.onclick = function() {
+            var index = parseInt(this["data-index"]);
+            if ( isDir ) {
+              pf.mlibrary.path += list[index] + "/";
+              pf.mlibrary.selected = null;
+            } else {
+              pf.mlibrary.selected[index] = ! pf.mlibrary.selected[index];
+            }
+            pf.mlibrary.renderLinks();
+          }
+          div.appendChild(a);
+        }
+        if ( list.length <= 0 ) {
+          var p = document.createElement("p");
+          p.innerText = "No songs in album!";
+          div.appendChild(p);
+          document.getElementById("mlibrary-button1").disabled = "disabled";
+          document.getElementById("mlibrary-button2").disabled = "disabled";
+        } else {
+          document.getElementById("mlibrary-button1").disabled = "";
+          document.getElementById("mlibrary-button2").disabled = "";
+        }
+      }
       fs.readdir(`${DATA_FOLDER}/music/${pf.mlibrary.path}`,function(err,list) {
         if ( err ) throw err;
-        fs.stat(`${DATA_FOLDER}/music/${pf.mlibrary.path}/${list[0]}`,function(err,stats) {
-          var isDir = stats.isDirectory();
-          if ( ! isDir ) {
-            if ( ! pf.mlibrary.selected ) pf.mlibrary.selected = list.map(item => false);
-            document.getElementById("mlibrary-button1").style.display = "inline";
-            document.getElementById("mlibrary-button2").style.display = "inline";
-            if ( pf.mlibrary.selected.filter(item => ! item).length > 0 ) document.getElementById("mlibrary-button1").innerText = "Select All";
-            else document.getElementById("mlibrary-button1").innerText = "Deselect All";
-          } else {
-            document.getElementById("mlibrary-button1").style.display = "none";
-            document.getElementById("mlibrary-button2").style.display = "none";
-          }
-          var div = document.getElementById("mlibrary-items");
-          while ( div.firstChild ) {
-            div.removeChild(div.firstChild);
-          }
-          for ( var i = 0; i < list.length; i++ ) {
-            var a = document.createElement("a");
-            if ( ! isDir ) {
-              for ( var j = 0; j < list[i].length; j++ ) {
-                if ( "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".indexOf(list[i].charAt(j)) > -1 ) break;
-              }
-              list[i] = list[i].slice(j).split(".").slice(0,-1).join(".");
-            }
-            a.innerText = list[i];
-            if ( ! isDir ) a.style.color = pf.mlibrary.selected[i] ? "#00aa00" : "blue";
-            a["data-index"] = i;
-            a.onclick = function() {
-              var index = parseInt(this["data-index"]);
-              if ( isDir ) {
-                pf.mlibrary.path += list[index] + "/";
-                pf.mlibrary.selected = null;
-              } else {
-                pf.mlibrary.selected[index] = ! pf.mlibrary.selected[index];
-              }
-              pf.mlibrary.renderLinks();
-            }
-            div.appendChild(a);
-          }
-        });
+        if ( list.length > 0 ) {
+          fs.stat(`${DATA_FOLDER}/music/${pf.mlibrary.path}/${list[0]}`,function(err,stats) {
+            var isDir = stats.isDirectory();
+            merge(list,isDir);
+          });
+        } else {
+          merge(list,false);
+        }
       });
     }
   },
@@ -414,7 +430,7 @@ var pf = { // Page Functions
   }
 }
 
-var ytPlayer;
+var aa,ytPlayer;
 class AudioAgent {
   constructor() {
     this.audio = document.getElementById("audio");
