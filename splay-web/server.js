af0e8dc332a16bf2752f91cf96101f0092cc37a2
Simas2006 <sjuknelis@gmail.com>
2019-07-08 22:30:37 +0300

    Add system to request music file from server

--- splay-web/server.js
+++ splay-web/server.js
@@ -3,10 +3,12 @@ var express = require("express");
 var app = express();
 var PORT = process.argv[2] || 8000;
 var DATA_FOLDER = __dirname + "/../splay-tv/data";
+var pendingRequests = {};
 
 app.use("/public",express.static(__dirname + "/public"));
 
 app.get("/data_request",function(request,response) {
+  console.log(request.ip,request.query.type);
   if ( request.query.type == "list_music" ) {
     var path = request.query.param;
     fs.readdir(`${DATA_FOLDER}/music/${path}`,function(err,files) {
@@ -20,6 +22,30 @@ app.get("/data_request",function(request,response) {
         }));
       });
     });
+  } else if ( request.query.type == "get_music_file" ) {
+    var id = Math.floor(Math.random() * 1e16);
+    while ( pendingRequests[id] ) {
+      id = Math.floor(Math.random() * 1e16);
+    }
+    pendingRequests[id] = {
+      "type": "music",
+      "path": request.query.param,
+      "ip": request.ip
+    };
+    response.send(id.toString());
+  }
+});
+
+app.get("/large_data_request",function(request,response) {
+  if ( pendingRequests[request.query.id] ) {
+    var dataReq = pendingRequests[request.query.id];
+    if ( request.ip != dataReq.ip ) {
+      response.send("Error");
+      return;
+    }
+    var stream = fs.createReadStream(`${DATA_FOLDER}/${dataReq.type}/${dataReq.path}`);
+    response.set("Content-Type","audio/mp4");
+    stream.pipe(response);
   }
 });
 
